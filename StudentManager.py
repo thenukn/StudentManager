823c564dcfe3455687bae1d0b0c4c71173a71275
Thenuk Nethmitha <thenuknethmitha2009@gmail.com>
2026-01-23 12:06:23 +0530

    created file

--- StudentManager.py
+++ StudentManager.py
@@ -1,5 +1,8 @@
-print('hi')
-print('hi')
-print('hi')
-print('hi')
-print('hi')
+import os
+class StudentManager:
+    def create(self, filename="students.txt"):
+        """Initializes and creates the data file if it doesn't exist."""
+        self.filename = filename
+        if not os.path.exists(self.filename):
+            # Create an empty file if it's missing
+            open(self.filename, 'w').close()
